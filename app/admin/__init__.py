34de9ea7d6b5c29e01d861120ee8c2bd4186d825
Patrick Rodrigues <patrick.pwall@gmail.com>
2019-06-15 00:24:51 -0300

    Adds a custom user model and user model admin

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .. import models
 
-# Import your registered models here.
+# Import your custom admin classes here.
+from .user import UserAdmin
+
+
+admin.site.register(models.User, user.UserAdmin)
