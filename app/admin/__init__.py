59dcfbd63157b05f7347e309508de87c51d34829
Patrick Rodrigues <patrick.pwall@gmail.com>
2019-06-15 01:56:26 -0300

    Removes UserAdmin

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .. import models
 
 # Import your custom admin classes here.
-from .user import UserAdmin
 
 
-admin.site.register(models.User, user.UserAdmin)
+admin.site.register(models.User)
